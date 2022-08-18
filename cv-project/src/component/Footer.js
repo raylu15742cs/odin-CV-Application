9bdfcc0846fce395f3ca09582625962b74ddde7f
Raymond Lu <raylucs15742@gmail.com>
2022-08-17 21:17:33 -0700

    update logo:

--- cv-project/src/component/Footer.js
+++ cv-project/src/component/Footer.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Footer = (props) => {
   return (
-    <div className='Footer'>
+    <div className="Footer">
       <p>
-        Copyright &copy; 2022 Raymond Lu {" "}
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZkibQaj5LLhxte7YsqiitCq7pn7T8BZksWb_nZpaPKw&s"
-          className='githublogo'
-          alt="github"
-          height="13px"
-        ></img>
+        Copyright &copy; 2022 Raymond Lu{' '}
+        <a href="https://github.com/raylu15742cs" target="#">
+          <img
+            src="https://spng.pinpng.com/pngs/s/256-2562212_github-logo-png-github-svg-logo-transparent-png.png"
+            className="githublogo"
+            alt="github"
+            height="15px"
+        
+          ></img>
+        </a>
       </p>
     </div>
   );
